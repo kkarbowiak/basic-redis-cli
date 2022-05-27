baec02806767b8add0b160784d95e7012dd22c14
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-05-27 14:39:26 +0200

    Cleanup: fix typo

--- redis_client.cpp
+++ redis_client.cpp
@@ -36,7 +36,7 @@ RedisClient::RedisClient(std::string const & ip, int port)
 {
     if (!m_redis_context)
     {
-        throw std::runtime_error("connection error: could not allocate Redis context");
+        throw std::runtime_error("Connection error: could not allocate Redis context");
     }
     if (m_redis_context->err)
     {
